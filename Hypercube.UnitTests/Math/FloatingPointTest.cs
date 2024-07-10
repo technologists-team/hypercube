998748e6e7db208467ee5bae941ff6688f2fac4b
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-07-11 02:53:56 +1000

    Added FloatingPointEquals tests

---
+++ Hypercube.UnitTests/Math/FloatingPointTest.cs
@@ -0,0 +1,15 @@
+﻿using Hypercube.Shared.Math.Extensions;
+
+namespace Hypercube.UnitTests.Math;
+
+public class FloatingPointTest
+{
+    [Test]
+    public void Equals()
+    {
+        Assert.That((0.1d + 0.2d).AboutEquals(0.3d));
+        Assert.That((0.1f + 0.2f).AboutEquals(0.3f));
+        
+        Assert.Pass($"{nameof(FloatingPointEqualsExtension)} passed");
+    }
+}
