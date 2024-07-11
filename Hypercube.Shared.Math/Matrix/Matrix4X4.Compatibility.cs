2f2f7ca5d057322c240a384b247db7367d35301d
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-07-12 08:25:12 +1000

    Remove using OpenTK.Mathematics for ompatibilities

--- Hypercube.Shared.Math/Matrix/Matrix4X4.Compatibility.cs
+++ Hypercube.Shared.Math/Matrix/Matrix4X4.Compatibility.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-using OpenTK.Mathematics;
 
 namespace Hypercube.Shared.Math.Matrix;
 
@@ -15,7 +14,7 @@ public partial struct Matrix4X4
         return new OpenTK.Mathematics.Matrix4(matrix4X4.Row0, matrix4X4.Row1, matrix4X4.Row2, matrix4X4.Row3);
     }
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static implicit operator Matrix4X4(Matrix4 matrix4)
+    public static implicit operator Matrix4X4(OpenTK.Mathematics.Matrix4 matrix4)
     {
         return new Matrix4X4(matrix4.Row0, matrix4.Row1, matrix4.Row2, matrix4.Row3);
     }
