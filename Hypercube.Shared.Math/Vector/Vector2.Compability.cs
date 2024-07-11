1f4145ef802d6f5132203c60b9db034e98b4ce90
JerryImMouse <toperkas@gmail.com>
2024-07-12 02:28:38 +0500

    add compatibility operators

--- Hypercube.Shared.Math/Vector/Vector2.Compability.cs
+++ Hypercube.Shared.Math/Vector/Vector2.Compability.cs
@@ -4,11 +4,19 @@ namespace Hypercube.Shared.Math.Vector;
 
 public readonly partial struct Vector2
 {
+    /*
+     * Self Compatibility
+     */
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static implicit operator Vector3(Vector2 vector)
     {
         return new Vector3(vector.X, vector.Y, 0f);
     }
+    
+    /*
+     * System.Numerics Compatibility
+     */
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static implicit operator Vector2(System.Numerics.Vector2 vector)
@@ -22,6 +30,10 @@ public readonly partial struct Vector2
         return new System.Numerics.Vector2(vector.X, vector.Y);
     }
     
+    /*
+     * OpenTK Compatibility
+     */
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static implicit operator Vector2(OpenTK.Mathematics.Vector2 vector)
     {
@@ -32,5 +44,21 @@ public readonly partial struct Vector2
     public static implicit operator OpenTK.Mathematics.Vector2(Vector2 vector)
     {
         return new OpenTK.Mathematics.Vector2(vector.X, vector.Y);
+    }
+    
+    /*
+     * OpenToolkit Compatibility
+     */
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator Vector2(OpenToolkit.Mathematics.Vector2 vector)
+    {
+        return new Vector2(vector.X, vector.Y);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator OpenToolkit.Mathematics.Vector2(Vector2 vector)
+    {
+        return new OpenToolkit.Mathematics.Vector2(vector.X, vector.Y);
     }
 }
--- Hypercube.Shared.Math/Vector/Vector3.Compability.cs
+++ Hypercube.Shared.Math/Vector/Vector3.Compability.cs
@@ -4,9 +4,61 @@ namespace Hypercube.Shared.Math.Vector;
 
 public readonly partial struct Vector3
 {
+    /*
+     * Vector2 Compatibility
+     */
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static implicit operator Vector2(Vector3 vector)
     {
         return new Vector2(vector.X, vector.Y);
+    }
+    
+    /*
+     * System.Numerics Compatibility
+     */
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator System.Numerics.Vector3(Vector3 vector3)
+    {
+        return new System.Numerics.Vector3(vector3.X, vector3.Y, vector3.Z);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator Vector3(System.Numerics.Vector3 vector3)
+    {
+        return new Vector3(vector3.X, vector3.Y, vector3.Z);
+    }
+    
+    /*
+     * OpenTK Compatibility
+     */
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator OpenTK.Mathematics.Vector3(Vector3 vector)
+    {
+        return new OpenTK.Mathematics.Vector3(vector.X, vector.Y, vector.Z);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator Vector3(OpenTK.Mathematics.Vector3 vector)
+    {
+        return new Vector3(vector.X, vector.Y, vector.Z);
+    }
+    
+    /*
+     * OpenToolkit Compatibility
+     */
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator OpenToolkit.Mathematics.Vector3(Vector3 vector)
+    {
+        return new OpenToolkit.Mathematics.Vector3(vector.X, vector.Y, vector.Z);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator Vector3(OpenToolkit.Mathematics.Vector3 vector)
+    {
+        return new Vector3(vector.X, vector.Y, vector.Z);
     }
 }
