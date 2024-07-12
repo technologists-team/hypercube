7ec1c555ce0ad3d08298036b32cd3f2613926324
JerryImMouse <toperkas@gmail.com>
2024-07-13 04:22:34 +0500

    create own texparamname

---
+++ Hypercube.Client/Utilities/TextureHelper.cs
@@ -0,0 +1,12 @@
+﻿using OpenToolkit.Graphics.OpenGL4;
+
+namespace Hypercube.Client.Utilities;
+
+public static class TextureHelper
+{
+    public static TextureParameterName ToOpenToolkit(
+        this Graphics.Texturing.TextureSettings.TextureParameterName textureParameterName)
+    {
+        return (TextureParameterName)(int)textureParameterName;
+    }
+}
