ac3d91be4d2be18e8d03b8f6b681cabb0069ff54
JerryImMouse <toperkas@gmail.com>
2024-07-13 03:49:19 +0500

    resolve conflicts

--- Hypercube.Shared/EventBus/Handlers/RefHandler.cs
+++ Hypercube.Shared/EventBus/Handlers/RefHandler.cs
@@ -1,3 +1,5 @@
-﻿namespace Hypercube.Shared.EventBus.Handlers;
+﻿using Hypercube.Shared.Utilities.Units;
+
+namespace Hypercube.Shared.EventBus.Handlers;
 
 public delegate void RefHandler(ref Unit ev);
