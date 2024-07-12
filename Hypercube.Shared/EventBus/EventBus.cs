5b72ca42a32b60c52e7366a86e6c992d2b141cb9
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-07-12 10:20:07 +1000

    Closes #10; Fixied EventBus GC bug

--- Hypercube.Shared/EventBus/EventBus.cs
+++ Hypercube.Shared/EventBus/EventBus.cs
@@ -1,38 +1,36 @@
 ﻿namespace Hypercube.Shared.EventBus;
 
+// TODO: BURNING IN HELL SHIT!!!! Rewrite it
 public class EventBus : IEventBus
 {
-    private readonly Dictionary<Type, HashSet<WeakReference<Delegate>>> _callbacks = new();
+    private readonly Dictionary<Type, HashSet<Delegate>> _callbacks = new();
     
     public void Subscribe<T>(Action<T> callback)
     {
-        GetListeners<T>().Add(new WeakReference<Delegate>(callback));
+        GetListeners<T>().Add(callback);
     }
 
     public void Unsubscribe<T>(Action<T> callback)
     {
-        GetListeners<T>().Remove(new WeakReference<Delegate>(callback));
+        GetListeners<T>().Remove(callback);
     }
 
     public void Invoke<T>(T signal)
     {
         foreach (var reference in GetListeners<T>())
         {
-            if (!reference.TryGetTarget(out var @delegate))
-                continue;
-            
-            if (@delegate is not Action<T> action)
+            if (reference is not Action<T> action)
                 continue;
             
             action.Invoke(signal);
         }
     }
 
-    private HashSet<WeakReference<Delegate>> GetListeners<T>()
+    private HashSet<Delegate> GetListeners<T>()
     {
         if (_callbacks.TryGetValue(typeof(T), out var listeners))
             return listeners;
 
-        return _callbacks[typeof(T)] = new HashSet<WeakReference<Delegate>>();
+        return _callbacks[typeof(T)] = new HashSet<Delegate>();
     }
 }
