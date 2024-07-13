c1dc7db2b92217ca240c1599c73c49c8297d03af
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-07-14 05:22:21 +1000

    Added audio laoder

---
+++ Hypercube.Client/Audio/Loading/AudioLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Frozen;
+using Hypercube.Client.Audio.Loading.Type;
+
+namespace Hypercube.Client.Audio.Loading;
+
+public sealed class AudioLoader : IAudioLoader
+{
+    private readonly FrozenDictionary<AudioType, IAudioTypeLoader> _loaders = new Dictionary<AudioType, IAudioTypeLoader>
+    {
+        { AudioType.Wav, new AudioWavLoader() }
+    }.ToFrozenDictionary();
+
+
+    public IAudioData LoadAudioData(Stream stream, AudioType type)
+    {
+        return _loaders[type].LoadAudioData(stream);
+    }
+}
---
+++ Hypercube.Client/Audio/Loading/IAudioTypeLoader.cs
@@ -0,0 +1,8 @@
+﻿using Hypercube.Client.Audio.Reader;
+
+namespace Hypercube.Client.Audio.Loading;
+
+public interface IAudioTypeLoader
+{
+    IAudioData LoadAudioData(Stream stream);
+}
