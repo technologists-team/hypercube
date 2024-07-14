416c24568879854b77a512740d6bd0188311bc33
JerryImMouse <toperkas@gmail.com>
2024-07-14 07:31:56 +0500

    fix audio source caching

--- Hypercube.Client/Resources/Caching/AudioSourceResource.cs
+++ Hypercube.Client/Resources/Caching/AudioSourceResource.cs
@@ -5,19 +5,14 @@ using Hypercube.Shared.Resources.Caching.Resource;
 
 namespace Hypercube.Client.Resources.Caching;
 
-public sealed class AudioSourceResource : Resource, IDisposable
+public sealed class AudioSourceResource : Resource
 {
     public ResourcePath Path;
-    public IAudioSource Stream;
+    public AudioStream Stream;
     
     public override void Load(ResourcePath path, DependenciesContainer container)
     {
         var audioMan = container.Resolve<IAudioManager>();
-        Stream = audioMan.CreateSource(path, new AudioSettings());
-    }
-
-    public void Dispose()
-    {
-        Stream.Dispose();
+        Stream = audioMan.GetAudio(path, new AudioSettings());
     }
 }
