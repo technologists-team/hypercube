f297fccde2bcd52e6cf9862faabe6a91457d44f4
JerryImMouse <toperkas@gmail.com>
2024-07-16 03:31:06 +0500

    some logger changes

--- Hypercube.Shared/Logging/LoggingManager.cs
+++ Hypercube.Shared/Logging/LoggingManager.cs
@@ -2,8 +2,13 @@
 
 public static class LoggingManager
 {
+    private static readonly Dictionary<string, Logger> CachedLoggers = new();
+    
     public static Logger GetLogger(string name)
     {
-        return new Logger(name);
+        if (CachedLoggers.TryGetValue(name, out var logger))
+            return logger;
+
+        return CachedLoggers[name] = new Logger(name);
     }
 }
