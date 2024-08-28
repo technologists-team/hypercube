2f792f17b9bfb0ae164c69b61d63fcb93f7a92b0
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-08-29 08:35:17 +1000

    Update vector2
    Updating the vector according to the given pattern https://github.com/technologists-team/hypercube/issues/51

--- Hypercube.Mathematics/Vectors/Vector2.cs
+++ Hypercube.Mathematics/Vectors/Vector2.cs
@@ -9,6 +9,8 @@ namespace Hypercube.Mathematics.Vectors;
 public readonly partial struct Vector2 : IEquatable<Vector2>
 {
     public static readonly Vector2 NaN = new(float.NaN, float.NaN);
+    public static readonly Vector2 PositiveInfinity = new(float.PositiveInfinity, float.PositiveInfinity);
+    public static readonly Vector2 NegativeInfinity = new(float.NegativeInfinity, float.NegativeInfinity);
     public static readonly Vector2 Zero = new(0, 0);
     public static readonly Vector2 One = new(1, 1);
     
@@ -47,7 +49,33 @@ public readonly partial struct Vector2 : IEquatable<Vector2>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => MathF.Atan2(Y, X);
     }
+
+    public float Summation
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => X + Y;
+    }
+
+    public float Production 
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => X * Y;
+    }
     
+    public float this[int index]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            return index switch
+            {
+                0 => X,
+                1 => Y,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+    }
+
     public Vector2(float x, float y)
     {
         X = x;
@@ -91,32 +119,15 @@ public readonly partial struct Vector2 : IEquatable<Vector2>
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public Vector2 Rotate(float angle)
-    {
-        var cos = MathF.Cos(angle);
-        var sin = MathF.Sin(angle);
-        
-        return new Vector2(
-            cos * X - sin * Y,
-            sin * X + cos * Y);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public float DistanceSquared(Vector2 other)
     {
         return (this - other).LengthSquared;
     }
-    
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public float Distance(Vector2 other)
     {
         return MathF.Sqrt(DistanceSquared(this, other));
-    }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public float Dot(Vector2 other)
-    {
-        return Dot(this, other);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -124,7 +135,65 @@ public readonly partial struct Vector2 : IEquatable<Vector2>
     {
         return Cross(this, other);
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public float Dot(Vector2 other)
+    {
+        return Dot(this, other);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Max(Vector2 other)
+    {
+        return Max(this, other);
+    }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Min(Vector2 other)
+    {
+        return Min(this, other);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Abs()
+    {
+        return Abs(this);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Round()
+    {
+        return Round(this);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Round(int digits)
+    {
+        return Round(this, digits);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Ceiling()
+    {
+        return Ceiling(this);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Floor()
+    {
+        return Floor(this);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Vector2 Rotate(float angle)
+    {
+        var cos = MathF.Cos(angle);
+        var sin = MathF.Sin(angle);
+        return new Vector2(
+            cos * X - sin * Y,
+            sin * X + cos * Y);
+    }
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool Equals(Vector2 other)
     {
@@ -251,23 +320,7 @@ public readonly partial struct Vector2 : IEquatable<Vector2>
     {
         return MathF.Sqrt(DistanceSquared(a, b));
     }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Vector2 Max(Vector2 a, Vector2 b)
-    {
-        return new Vector2(
-            MathF.Max(a.X, b.X),
-            MathF.Max(a.Y, b.Y));
-    }
-    
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Vector2 Min(Vector2 a, Vector2 b)
-    {
-        return new Vector2(
-            MathF.Min(a.X, b.X),
-            MathF.Min(a.Y, b.Y));
-    }
-    
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float Dot(Vector2 a, Vector2 b)
     {
@@ -278,5 +331,69 @@ public readonly partial struct Vector2 : IEquatable<Vector2>
     public static float Cross(Vector2 a, Vector2 b)
     {
         return a.X * b.Y - a.Y * b.X;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Max(Vector2 a, Vector2 b)
+    {
+        return new Vector2(
+            MathF.Max(a.X, b.X),
+            MathF.Max(a.Y, b.Y));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Min(Vector2 a, Vector2 b)
+    {
+        return new Vector2(
+            MathF.Min(a.X, b.X),
+            MathF.Min(a.Y, b.Y));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Abs(Vector2 vector)
+    {
+        return new Vector2(
+            Math.Abs(vector.X),
+            Math.Abs(vector.Y));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Round(Vector2 vector)
+    {
+        return new Vector2(
+            Math.Round(vector.X),
+            Math.Round(vector.Y));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Round(Vector2 vector, int digits)
+    {
+        return new Vector2(
+            Math.Round(vector.X, digits),
+            Math.Round(vector.Y, digits));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Ceiling(Vector2 vector)
+    {
+        return new Vector2(
+            Math.Ceiling(vector.X),
+            Math.Ceiling(vector.Y));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Floor(Vector2 vector)
+    {
+        return new Vector2(
+            Math.Floor(vector.X),
+            Math.Floor(vector.Y));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Sign(Vector2 vector)
+    {
+        return new Vector2(
+            Math.Sign(vector.X),
+            Math.Sign(vector.Y));
     }
 }
