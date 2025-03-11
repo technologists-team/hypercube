45adaaa7add45cd425598b0dff05275879781d53
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2025-03-12 01:48:50 +1000

    fix: RegexPattern in EntitySystemNamingConventionAnalyzer

--- src/Hypercube.Analyzers/EntitySystemNamingConventionAnalyzer.cs
+++ src/Hypercube.Analyzers/EntitySystemNamingConventionAnalyzer.cs
@@ -10,5 +10,5 @@ public sealed class EntitySystemNamingConventionAnalyzer : NamingConventionAnaly
     protected override LocalizableString Title => "EntitySystem naming convention";
     protected override LocalizableString MessageFormat => "Class {0} marked with [RegisterEntitySystem] must end with System or EntitySystem";
     protected override string AttributeName => "RegisterEntitySystemAttribute";
-    protected override string RegexPattern => "Component$";
+    protected override string RegexPattern => "(System|EntitySystem)$";
 }
