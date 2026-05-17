ff5e05ad183acf876bb4dee5e13415f97ca14f73
Tesmi <88522894+Tesmi-Develop@users.noreply.github.com>
2026-05-17 16:49:37 +1000

    fix: added implementation IWorld.Dynamic

---
+++ src/Hypercube.Core/Ecs/EntitySystemManager.WorldDynamic.cs
@@ -0,0 +1,31 @@
+﻿using Hypercube.Ecs;
+
+namespace Hypercube.Core.Ecs;
+
+public partial class EntitySystemManager
+{
+    public object Add(Entity entity, Type type)
+    {
+        return _globalWorld.Add(entity, type);
+    }
+
+    public void Add(Entity entity, object? value)
+    {
+        _globalWorld.Add(entity, value);
+    }
+
+    public object Get(Entity entity, Type type)
+    {
+        return _globalWorld.Get(entity, type);
+    }
+
+    public bool Has(Entity entity, Type type)
+    {
+        return _globalWorld.Has(entity, type);
+    }
+
+    public void Remove(Entity entity, Type type)
+    {
+        _globalWorld.Remove(entity, type);
+    }
+}
